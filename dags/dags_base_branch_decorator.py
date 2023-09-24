050acf1024544e08c03d47aa7aca9c75c7ad6e58
Leeyua-airim <brink9205@gmail.com>
2023-09-24 14:48:47 +0900

    bashbranch

--- dags/dags_base_branch_decorator.py
+++ dags/dags_base_branch_decorator.py
@@ -7,34 +7,37 @@ from airflow.operators.bash import BashOperator
 from airflow.models import Variable
 from airflow.operators.python import BranchPythonOperator
 from airflow.operators.python import PythonOperator
+from airflow.operators.branch import BaseBranchOperator
 
 with DAG(
-    dag_id='dags_python_woth_branch_decorator',
+    dag_id='dags_base_branch_decorator',
     start_date = pendulum.datetime(2023, 9, 1, tz="Asia/Seoul"),
     schedule   = None,
     catchup    = False
 ) as dag:
-    # 분기처리를 위한 데코레이터 / 래핑
-    @task.branch(task_id = 'python_branch_task')
-    def select_random():
-        import random # 필요한 라이브러리는 이렇게 내부에 할당 
+    # BaseBranchOpertor 를 사용한 분기처리의 경우 다음과 같이 상속을 처리해야함
+    # class 명칭은 커스텀 가능 
+    class CustomBranchOperator(BaseBranchOperator):
+        
+        # choose_branch() 함수 명칭은 그대로 써야함 
+        # context 도 그대로 써야함
+        # python 오퍼레이터 쓸 때 
+        def choose_branch(self, context):
+            import random
+            
+            print("context : ", context)
 
-        item_lst     = ['A', 'B', 'C']
-        selected_item = random.choice(item_lst)
-        # 위 로직에서 A 가 선택시 'task_a' 를 반환 
-        if selected_item == 'A':
-            return 'task_a' # 실행되어야 하는 테스크 아이디값으로 반환되어야 함
-        elif selected_item in ['B','C']: #선택된 값이 'B', C 인 경우 
-            return ['task_b', 'task_c']
+            item_lst = ['A', 'B', 'C']
+            selected_item = random.choice(item_lst)
+            if selected_item == 'A':
+                return 'task_a'
+            elif selected_item in ['B','C']:
+                return ['task_b','task_c']
+            
+    custom_branch_operator = CustomBranchOperator(task_id='python_branch_task')
 
-    # # 분기를 처리하는 메서드 branchpythonoperator
-    # python_branch_task = BranchPythonOperator(
-    #     task_id = 'python_branch_task',
-    #     python_callable = select_random # python_callable 을 통해 매개변수 전달,
-    # )
-
-    def common_func(**kwargs):
-        print(kwargs['selected'])
+def common_func(**kwargs):
+    print(kwargs['selected'])
 
     task_a = PythonOperator(
         task_id = 'task_a',
@@ -55,4 +58,4 @@ with DAG(
     )
 
     # 태스크 실행 -> 세 테스크 후보 중 하나가 실행되어야 하므로, 다음과 같이 리스트로 묶어줌 
-    select_random() >> [task_a, task_b, task_c]
+    custom_branch_operator >> [task_a, task_b, task_c]
