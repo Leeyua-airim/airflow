d1b36a18d391195c3a9d1df7aabda6b3fd34df9e
Leeyua-airim <brink9205@gmail.com>
2023-09-25 20:49:43 +0900

    grouping

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -43,4 +43,17 @@ with DAG(
 
         inner_func1() >> inner_function2
 
-    group_1()
+    with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다.') as group_2:
+        @task(task_id='inner_funciton1')
+        def inner_func1(**kwargs):
+            print(" 두 번째 TaskGroup 내 첫 번째 task 입니다.")
+
+        inner_function2 = PythonOperator(
+            task_id = "inner_function2",
+            python_callable=inner_func,
+            op_kwargs={'msg' : '두 번째 taskGroup 내 두 번째 task 입니다.'}
+        )
+
+        inner_func1() >> inner_function2
+
+    group_1() >> group_2
